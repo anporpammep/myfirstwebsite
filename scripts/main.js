0b45fde500bf4622a35c9dc3abca9c29ebc8320d
Galina Jarkich <galina.jarkich@lawpro.ca>
2025-04-21 15:12:14 -0400

    Added button to change a name.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 const myImage = document.querySelector("img");
+const noName = "I am snow leopard without a name.";
 
 myImage.addEventListener("click", () => {
     const mySource = myImage.getAttribute("src");
@@ -8,3 +9,29 @@ myImage.addEventListener("click", () => {
         myImage.setAttribute("src","/images/snow-leopard-main.png")
     }
 })
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    let myName = prompt("Please enter my new name.");
+    if(!myName) {
+        myHeading.textContent = `I am snow leopard without a name.`;
+        myImage.setAttribute("src","/images/snow-leopard-main.png")
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Hello, my name is ${myName}`;
+        myImage.setAttribute("src","/images/snow-leopard-side.png")
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Hello, my name is ${storedName}`;
+    myImage.setAttribute("src","/images/snow-leopard-side.png")
+}
+
+myButton.addEventListener("click", () => {
+    setUserName()});
